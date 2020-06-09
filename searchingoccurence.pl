475bfe1977bf4d15d41e1d99385c2fdb093f372a
Navneet Tyagi <tyagibijnor@gmail.com>
2020-06-09 22:06:14 +0530

    Searching text.

---
+++ searchingoccurence.pl
@@ -0,0 +1,20 @@
+use strict;
+use warnings;
+
+$|=1;
+
+sub main {
+	my $input = 'D:\xampp\cgi-bin\perlcoding\perlcode\test.txt';
+	open( INPUT ,$input) or die("Input $input not found \n");
+	
+	while( my $line = <INPUT>){
+		
+		if($line =~ /(T..s...)/) { # searching word T**s
+			print "$1\n";		
+		}
+	}
+	close(INPUT);
+	
+}
+
+main();
