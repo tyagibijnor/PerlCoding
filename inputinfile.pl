3d1b69f9c812ec2120a67b4027039278daf6f0f0
Navneet Tyagi <tyagibijnor@gmail.com>
2020-06-09 16:59:28 +0530

    Change or condition

--- inputinfile.pl
+++ inputinfile.pl
@@ -5,8 +5,7 @@ $|=1;
 
 sub main {
 	my $file = 'D:\xampp\cgi-bin\perlcoding\perlcode\test.txt';
-	open( INPUT ,$file);
-	die "Input $file not found \n";
+	open( INPUT ,$file) or die("Input $file not found \n");
 	close( INPUT );
 }
 
