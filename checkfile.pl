c0e4ee281d5e1daef0e2aca84ed6924be60fa01d
Navneet Tyagi <tyagibijnor@gmail.com>
2020-06-09 15:12:17 +0530

    new line comment added.

--- checkfile.pl
+++ checkfile.pl
@@ -4,10 +4,10 @@ use warnings;
 sub main {
 	my $file = 'D:\xampp\cgi-bin\perlcoding\perlcode\checkfile.pl'; 
 	if(-f $file ) {
-		print "File founded";
+		print "File founded \n";
 	}
 	else {
-		print "File is not founded";
+		print "File is not founded \n";
 	}
 }
 
