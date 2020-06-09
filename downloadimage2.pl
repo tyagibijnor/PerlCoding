5a846a8dc4b6f1bb12678a8ef6aacf6d22bd1bd9
Navneet Tyagi <tyagibijnor@gmail.com>
2020-06-09 14:13:03 +0530

    Added new line code.

--- downloadimage2.pl
+++ downloadimage2.pl
@@ -4,12 +4,12 @@ use LWP::Simple;
 
 sub main{
 	print "Downloading....\n";
-	my $image = getstore('https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png','test1.png');
+	my $image = getstore('https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png','test2.png');
 	if($image == 200){
-		print "Success";
+		print "Success\n";
 	}
 	else{
-		print "Failed";
+		print "Failed\n";
 	}
 	  
 	
