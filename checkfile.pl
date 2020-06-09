d09e01497f43c4c0fd73ad73231b2dbb7322a9c3
Navneet Tyagi <tyagibijnor@gmail.com>
2020-06-09 15:13:26 +0530

    print result wiht file path

--- checkfile.pl
+++ checkfile.pl
@@ -4,10 +4,10 @@ use warnings;
 sub main {
 	my $file = 'D:\xampp\cgi-bin\perlcoding\perlcode\checkfile.pl'; 
 	if(-f $file ) {
-		print "File founded \n";
+		print "File founded $file \n";
 	}
 	else {
-		print "File is not founded \n";
+		print "File is not founded $file \n";
 	}
 }
 
