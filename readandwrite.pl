934e4e9a297ac87166fe6bb0bc76eb433bee2c96
Navneet Tyagi <tyagibijnor@gmail.com>
2020-06-09 21:27:36 +0530

    Read and write test.

---
+++ readandwrite.pl
@@ -0,0 +1,22 @@
+use strict;
+use warnings;
+
+$|=1;
+
+sub main {
+	my $input = 'D:\xampp\cgi-bin\perlcoding\perlcode\test.txt';
+	open( INPUT ,$input) or die("Input $input not found \n");
+	my $output = 'output.txt';
+	open( OUTPUT ,'>'.$output) or die "Can't create $output \n";
+	
+	while( my $line = <INPUT>){
+		if($line =~ /test/) {
+		print OUTPUT "$line";		
+		}
+		
+	}
+	close(INPUT);
+	close(OUTPUT);
+}
+
+main();
