6f5761a554bbce81203580d837ec713e1a0d2a1e
Navneet Tyagi <tyagibijnor@gmail.com>
2020-06-10 14:15:26 +0530

    Finding number of preceding

--- rgnumeric.pl
+++ rgnumeric.pl
@@ -9,10 +9,11 @@ sub main {
 	# *? zero or more preceding character, as few as possible.
 	# +? one or more preceding character, as few as possible.
 	# {5} five of the preceding.
+	# {2,6} at least 2 and at most 6
 	
 	my $text = 'AQ85958899';
 	
-	if ($text =~ /(AQ\d{5})/) {
+	if ($text =~ /(AQ\d{2,8})/) {
 		print "Mathced : '$1'\n";
 	} 
 	else {
