3ea848678235dedad49c6bd58c495e59d754e267
Navneet Tyagi <tyagibijnor@gmail.com>
2020-06-11 20:16:26 +0530

    testing dumper

---
+++ usingdumper.pl
@@ -0,0 +1,21 @@
+use strict;
+use warnings;
+use Data::Dumper;
+$|=1;
+
+sub main {
+	
+	my $file = 'test.csv';
+	unless(open(INPUT, $file)) {
+		die "\n can not open file $file \n";
+	}
+	<INPUT>;
+	while (my $line = <INPUT>)
+	{
+		my @values = split ',',$line;
+		print $values[2]."\n";
+		
+	}	
+}
+
+main();
