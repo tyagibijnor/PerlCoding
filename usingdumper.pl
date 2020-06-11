5e29fe419044e2d561b855c5e8a3c9f1fb63c3ba
Navneet Tyagi <tyagibijnor@gmail.com>
2020-06-11 21:57:15 +0530

    using foreach to print.

--- usingdumper.pl
+++ usingdumper.pl
@@ -10,15 +10,22 @@ sub main {
 		die "\n can not open file $file \n";
 	}
 	<INPUT>;
+	my @lines;
+	my $count = 0;
 	while (my $line = <INPUT>)
 	{
 		chomp $line; 
 		my @values = split ',',$line;
 		#print $values[2]."\n";
-		print Dumper(@values);
-		
+		# print Dumper(@values);
+		$lines[$count] = $line;
+		$count++;			
 	}	
 	close INPUT;
+	
+	foreach my $l(@lines) {
+		print $l."\n";
+	}
 }
 
 main();
