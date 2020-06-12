89e9939a8219155056bac1584a5e110aa84c5a2c
Navneet Tyagi <tyagibijnor@gmail.com>
2020-06-12 17:06:19 +0530

    Pushing multiple array.

--- arraytest.pl
+++ arraytest.pl
@@ -4,10 +4,12 @@ use warnings;
 use Data::Dumper;
 
 my @friends = ('Ram','Shyam','Mohan');
+my @city = ('Moradabad','Delhi','Solan','Indore','Guggaon','Panchkulla');
 
 my @test;
 
 push @test,@friends;
+push @test,@city;
 
 print Dumper(@test);
 
