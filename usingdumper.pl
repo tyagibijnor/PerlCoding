5d8849917e432f568c302ab32fa3f392adb872fd
Navneet Tyagi <tyagibijnor@gmail.com>
2020-06-12 18:04:22 +0530

    printing dumper by index

--- usingdumper.pl
+++ usingdumper.pl
@@ -15,16 +15,19 @@ sub main {
 	while (my $line = <INPUT>)
 	{
 		chomp $line; 
-		my @values = split ',',$line;
+		my @values = split /\s*,\s*/,$line;
 		#print $values[2]."\n";
 		# print Dumper(@values);
-		$lines[$count] = $line;
-		$count++;			
+		push @lines,\@values;
+		#$lines[$count] = $line;
+		#$count++;			
 	}	
 	close INPUT;
+	#print $lines[3][0];
 	
 	foreach my $l(@lines) {
-		print $l. "\n";
+		#print Dumper($l);
+		print "Name : ".$l->[0]." , Role : ".$l->[2]."\n";
 	}
 }
 
