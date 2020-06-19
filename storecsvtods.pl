91e05ebadf823d1e7d99403c61ce33e67fd20bb0
Navneet Tyagi <tyagibijnor@gmail.com>
2020-06-19 17:01:51 +0530

    store csv to data structure

---
+++ storecsvtods.pl
@@ -0,0 +1,16 @@
+use strict;
+use warnings;
+
+$|=1;
+
+sub main {
+	
+	my $file = 'test.csv';
+	unless(open(INPUT, $file)) {
+		die "\n can not open file $file \n";
+	}
+	<INPUT>;
+	my @data;
+}
+
+main();
