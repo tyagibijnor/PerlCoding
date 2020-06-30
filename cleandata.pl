0014a2d048bdf10045f1a2b23b2fe68761763c3b
Navneet Tyagi <tyagibijnor@gmail.com>
2020-06-30 18:21:49 +0530

    Clean data from csv

---
+++ cleandata.pl
@@ -0,0 +1,32 @@
+use strict;
+use warnings;
+use Data::Dumper;
+$|=1;
+sub main {
+	my $file = 'newtest.csv';
+	unless(open(INPUT, $file)) {
+	die "\n can not open file $file \n";
+	}
+	<INPUT>;
+	my @data;
+	while ( my $line = <INPUT>){
+		chomp $line;
+		my @values = split(/,/, $line);
+		my ($name, $code, $profile) = @values;
+		my %result = (
+		"Name"=>$name,
+		"Code"=>$code,
+		"Profile"=>$profile,);
+		push @data, \%result; 
+	}
+	close INPUT;
+	foreach my $l(@data) {
+ 		#print Dumper($l);
+		print $l->{"Name"}."\n";
+		if($l->{"Name"} eq "Navneet"){
+			print "found";
+		}
+	}
+}
+
+main();
